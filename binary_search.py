960be32bead8c7aea6229c8b741913572734a780
InderdeepSync <buntycool1998@gmail.com>
2021-08-19 14:50:45 -0400

    SINGH-014: Shifted Binary Search

--- binary_search.py
+++ binary_search.py
@@ -18,7 +18,7 @@ def search_in_sorted_matrix(input_matrix, target, start, end):
     if start[0] > end[0] or start[1] > end[1]:
         return None
 
-    mid = (math.floor((start[0] + end[0])/2), math.floor((start[1] + end[1])/2))
+    mid = (math.floor((start[0] + end[0]) / 2), math.floor((start[1] + end[1]) / 2))
 
     current = input_matrix[mid[0]][mid[1]]
     if current == target:
@@ -28,6 +28,24 @@ def search_in_sorted_matrix(input_matrix, target, start, end):
     else:
         return search_in_sorted_matrix(input_matrix, target, (mid[0], mid[1]), end)
 
+
+def shifted_binary_search(arr, start, end, target):  # Verified on LeetCode
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] >= arr[start]:
+            if arr[mid] > target >= arr[start]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if arr[mid] < target <= arr[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+    return -1
 
 
 if __name__ == '__main__':
@@ -41,3 +59,5 @@ if __name__ == '__main__':
               [99, 100, 103, 106, 108, 1004]]
     found_coordinates = search_in_sorted_matrix(matrix, 44, start=(0, 0), end=(len(matrix) - 1, len(matrix[0]) - 1))
     print(found_coordinates)
+
+    print("Shifted Binary Search: {}".format(shifted_binary_search([3, 1], start=0, end=1, target=1)))
