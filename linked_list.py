1484a22fa059910341b4e46b166b1095df80fae1
InderdeepSync <buntycool1998@gmail.com>
2021-08-24 22:55:29 +0530

    SINGH-035: Linked List Algorithms

--- linked_list.py
+++ linked_list.py
@@ -39,6 +39,7 @@ def remove_duplicates_from_sorted_list(linked_list):
 
     return initial
 
+
 def shift_linked_list(linked_list: Node, k):
     k = k % len(linked_list)
 
@@ -62,6 +63,7 @@ def shift_linked_list(linked_list: Node, k):
 
     return initial
 
+
 def reverse_linked_list(linked_list):
     prev = linked_list
     current = linked_list.next
@@ -74,6 +76,7 @@ def reverse_linked_list(linked_list):
         current = temp
 
     return prev
+
 
 def merge_linked_lists(list1, list2):
     result_list = temp = Node("/")
@@ -98,19 +101,101 @@ def merge_linked_lists(list1, list2):
 
     return result_list.next
 
+
+def rearrange_linked_list(list1, k):
+    current = list1
+    less_start = less_than_k = Node("/")
+    more_start = more_than_k = Node("/")
+    equal_start = equal_to_k = Node("/")
+    while current:
+        temp_next = current.next
+        current.next = None
+
+        if current.value == k:
+            equal_to_k.next = current
+            equal_to_k = current
+        elif current.value < k:
+            less_than_k.next = current
+            less_than_k = current
+        else:
+            more_than_k.next = current
+            more_than_k = current
+
+        current = temp_next
+
+    equal_to_k.next = more_start.next
+    less_than_k.next = equal_start.next
+
+    return less_start.next
+
+
+def _split_linked_list(linked_list):
+    slow = fast = linked_list
+
+    while fast.next and fast.next.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    second_list = slow.next
+    slow.next = None
+    return linked_list, second_list
+
+def zip_linked_list(list1):
+    list1, list2 = _split_linked_list(list1)
+    list2 = reverse_linked_list(list2) if list2 else None
+
+    result_list = temp = Node("/")
+    while True:
+        if not list1:
+            break
+        temp.next = list1
+        list1 = list1.next
+        temp = temp.next
+
+        if not list2:
+            continue
+
+        temp.next = list2
+        list2 = list2.next
+        temp = temp.next
+
+    return result_list.next
+
+
+def linked_list_palindrome(linked_list):
+    list1, list2 = _split_linked_list(linked_list)
+    list2 = reverse_linked_list(list2) if list2 else None
+
+    while True:
+        if not list2:
+            return True
+
+        if list1.value != list2.value:
+            return False
+
+        list1 = list1.next
+        list2 = list2.next
+
+
 def main():
     linked_list = Node(1, Node(1, Node(2, Node(2, Node(3, Node(5, Node(5, Node(5))))))))
     print("Original: ", linked_list)
     linked_list = remove_duplicates_from_sorted_list(linked_list)
     print(linked_list)
 
-    linked_list2 = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8))))))))
-    print("Shifted Linked List: {}".format(shift_linked_list(linked_list2, 3)))
-    print("Reverse Linked List: {}".format(reverse_linked_list(linked_list2)))
+    linked_list2 = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8, Node(9)))))))))
+    # print("Shifted Linked List: {}".format(shift_linked_list(linked_list2, 3)))
+    # print("Reverse Linked List: {}".format(reverse_linked_list(linked_list2)))
 
     linked_list3 = Node(2, Node(6, Node(7, Node(8))))
-    linked_list4 = Node(1, Node(3, Node(4, Node(5, Node(9, Node(10))))))
-    print("Merge Sorted Linked Lists: {}".format(merge_linked_lists(linked_list3, linked_list4)))
+    linked_list4 = Node(1, Node(3, Node(4, Node(4, Node(3, Node(1))))))
+    # print("Merge Sorted Linked Lists: {}".format(merge_linked_lists(linked_list3, linked_list4)))
+
+    linked_list5 = Node(3, Node(0, Node(5, Node(3, Node(2, Node(1, Node(4, Node(-1))))))))
+    print("Rearrange Linked List: {}".format(rearrange_linked_list(linked_list5, 2.5)))
+
+    print("Zip Linked List: {}".format(zip_linked_list(linked_list2)))
+    print("Linked List Palindrome: {}".format(linked_list_palindrome(linked_list4)))
 
 
 if __name__ == "__main__":
