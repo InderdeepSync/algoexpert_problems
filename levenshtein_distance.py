e8d1476207901d1ef6fc61ebfca8e9fdb65448e7
InderdeepSync <buntycool1998@gmail.com>
2021-08-26 08:01:03 +0530

    SINGH-041: Levenshtein Distance Iterative

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -13,5 +13,21 @@ def levenshtein_distance(string1, string2):
         return 1 + min(temp1, levenshtein_distance(string1[1:], string2), levenshtein_distance(string1, string2[1:]))
 
 
+def levenshtein_distance_iterative(string1, string2):  # Verified on Leetcode
+    edits = [[x for x in range(len(string2) + 1)] for _ in range(len(string1) + 1)]
+
+    for i in range(1, len(string1) + 1):
+        edits[i][0] = edits[i - 1][0] + 1
+
+    for i in range(1, len(string1) + 1):
+        for j in range(1, len(string2) + 1):
+            if string1[i - 1] == string2[j - 1]:
+                edits[i][j] = edits[i - 1][j - 1]
+            else:
+                edits[i][j] = 1 + min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1])
+
+    return edits[-1][-1]
+
+
 if __name__ == "__main__":
-    print(levenshtein_distance("abc", "yabd"))
+    print(levenshtein_distance_iterative("abc", "abc"))
