fadecdbb24c80130291b5b3c99ad2bae946f7f07
InderdeepSync <buntycool1998@gmail.com>
2021-08-27 01:19:56 +0530

    SINGH-046: Spiral Traverse Elegant

--- spiral_traverse.py
+++ spiral_traverse.py
@@ -1,4 +1,3 @@
-
 def spiral_traverse(matrix):
     num_rows = len(matrix)
     num_columns = len(matrix[0])
@@ -12,7 +11,8 @@ def spiral_traverse(matrix):
 
     return res
 
-# Verified on LeetCode TODO Implement better approach
+
+# Verified on LeetCode
 def perimeter_func(matrix):
     transpose_matrix = list(zip(*matrix))
 
@@ -29,10 +29,45 @@ def _get_inner_matrix(matrix):
     return list(map(lambda row: row[1: -1], matrix[1: -1]))
 
 
+def spiral_traversal_elegant(matrix):  # Verified on LeetCode
+    num_rows = len(matrix)
+    num_columns = len(matrix[0])
+
+    row_upper = 0
+    row_lower = num_rows - 1
+
+    col_left = 0
+    col_right = num_columns - 1
+    spiral_result = []
+    while row_upper <= row_lower and col_left <= col_right:
+        if row_upper == row_lower:
+            spiral_result.extend(matrix[row_upper][col_left: col_right + 1])
+            break
+        elif col_left == col_right:
+            for row_index in range(row_upper, row_lower + 1):
+                spiral_result.append(matrix[row_index][col_left])
+            break
+
+        for i in range(col_left, col_right):
+            spiral_result.append(matrix[row_upper][i])
+        for i in range(row_upper, row_lower):
+            spiral_result.append(matrix[i][col_right])
+        for i in range(col_right, col_left, -1):
+            spiral_result.append(matrix[row_lower][i])
+        for i in range(row_lower, row_upper, -1):
+            spiral_result.append(matrix[i][col_left])
+
+        row_upper += 1
+        row_lower -= 1
+
+        col_left += 1
+        col_right -= 1
+
+    return spiral_result
 
 
 if __name__ == "__main__":
-    input_matrix = [[1,  2,  3,  4,  5],
+    input_matrix = [[1, 2, 3, 4, 5],
                     [16, 17, 18, 19, 6],
                     [15, 24, 25, 20, 7],
                     [14, 23, 22, 21, 8],
@@ -47,5 +82,5 @@ if __name__ == "__main__":
                      [0],
                      [1],
                      [1]]
-    result = spiral_traverse(input_matrix3)
+    result = spiral_traversal_elegant(input_matrix3)
     print(result)
