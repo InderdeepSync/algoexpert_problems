310a254ca0cbc7b4a85e9829315de7f6a1326633
InderdeepSync <buntycool1998@gmail.com>
2021-08-27 07:23:19 +0530

    SINGH-051: Longest Peak Algorithm Handle Edge Cases

--- longest_peak.py
+++ longest_peak.py
@@ -1,29 +1,21 @@
-
-
-def longest_peak(arr):
-    lengths = []
+def longest_peak(arr):  # Accepted on LeetCode
+    max_length = 0
     for i in range(1, len(arr) - 1):
-        if not arr[i-1] < arr[i] > arr[i+1]:
+        if not arr[i - 1] < arr[i] > arr[i + 1]:
             continue
 
         j = i + 2
-        while arr[j] < arr[j - 1] and j < len(arr):
+        while j < len(arr) and arr[j] < arr[j - 1]:
             j += 1
 
         k = i - 2
-        while arr[k] < arr[k+1] and k >= 0:
+        while k >= 0 and arr[k] < arr[k + 1]:
             k -= 1
 
-        lengths.append((j - 1) - (k + 1) + 1)  # final - initial + 1
+        max_length = max(max_length, (j - 1) - (k + 1) + 1)  # final - initial + 1
 
-    return max(lengths)
-
-
-
-
-
-
+    return max_length
 
 
 if __name__ == "__main__":
-    print(longest_peak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+    print(longest_peak([2, 1, 4, 7, 3, 2, 5]))
