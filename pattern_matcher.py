c361fed91f1083e15bdba4244c9ced01227d9b9a
Inderdeep Singh <inderdeep.singh@unscript.ai>
2024-04-01 00:28:52 +0530

    Pattern Matcher

--- pattern_matcher.py
+++ pattern_matcher.py
@@ -1,8 +1,40 @@
+import math
 
 
-def pattern_matcher(pattern, input_string):
-    pass
+def patternMatcher(pattern, string): # Verified on AlgoExpert
+    # Write your code here.
+    isSwapped = False
+    if pattern[0] == "y":
+        pattern = pattern.replace("x", "t").replace("y", "x").replace("t", "y")
+        isSwapped = True
+
+    if pattern.count("x") == len(string):
+        if len(string) % 4 != 0:
+            return False
+        x = string[0: len(string) // 4]
+        if string == "".join(map(lambda c: x, pattern)):
+            return [x, ""] if not isSwapped else ["", x]
+
+    xCount = pattern.count("x")
+    yCount = pattern.count("y")
+    xBeg = len(pattern) - len(pattern.lstrip('x'))
+    # yBeg = len(pattern.lstrip('x')) - len(pattern.lstrip('x').lstrip('y'))
+
+    for i in range(1, math.ceil(len(string) / xCount)):
+        x = string[:i]
+        yTotal = (len(string) - xCount * len(x))
+        if yTotal % yCount != 0:
+            continue
+
+        yLen = yTotal // yCount
+        y = string[xBeg * len(x): xBeg * len(x) + yLen]
+        joined = map(lambda c: x if c == "x" else y, pattern)
+
+        if "".join(joined) == string:
+            return [x, y] if not isSwapped else [y, x]
+
+    return []
 
 
 if __name__ == "__main__":
-    print(pattern_matcher("xxyxxy", "newnewyorknewnewyork"))
+    print(patternMatcher("xxyxxy", "gogopowerrangergogopowerranger"))
